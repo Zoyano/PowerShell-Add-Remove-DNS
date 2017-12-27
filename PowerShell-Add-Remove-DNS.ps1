80104c5e2501f8a92072d172c4267101fdd0eeee
Zoyano <34747586+Zoyano@users.noreply.github.com>
2017-12-27 15:40:47 -0600

    Update

--- PowerShell-Add-Remove-DNS.ps1
+++ PowerShell-Add-Remove-DNS.ps1
@@ -2,31 +2,35 @@ cls
 
 # Define Variables
 $global:InterfaceIndexNum = 0
+<#************** Enter List of DNS IP's here for Adding **************#>
+$global:DNSEntries = "('10.200.160.179', '10.200.160.173', '10.200.200.33', '10.200.200.32')"
 
-<# ************** Functions ************** #>
+<#************** Functions **************#>
 function GetInterfaceIndex {
     #Get-WmiObject win32_networkadapter -filter 'netconnectionstatus = 2' | select NetConnectionid, Name, Interfaceindex, NetConnectionStatus | Out-Host
     Get-NetAdapter | Where-Object Status -eq 'Up' | Select-Object Name, InterfaceDescription,  @{Name="Interfaceindex";Expression={$_."ifIndex"}} , Status | Out-Host
 
-    $global:InterfaceIndexNum = Read-Host "Which Interfaceindex do you want to update?"
+    $global:InterfaceIndexNum = Read-Host "Which Interfaceindex do you want to add DNS entries to?"
 }
 
-<# ************** Begin Process ************** #>
+<#************** Begin Process **************#>
 # Loops until $WhichAction equals A, R or E
 do {
     $WhichAction = Read-Host -Prompt 'Do you want to (A)dd/(R)emove DNS entries or (E)xit?'
 
+    #(A)dd DNS Entries
     if ($WhichAction -eq 'A') {
         Write-Host 'Starting process to Add DNS Entries...'
         GetInterfaceIndex
 
-        Set-DnsClientServerAddress -Interfaceindex $InterfaceIndexNum -ServerAddresses ('10.200.160.179', '10.200.160.173', '10.200.200.33', '10.200.200.32')
+        Set-DnsClientServerAddress -Interfaceindex $InterfaceIndexNum -ServerAddresses $global:DNSEntries
 
         Write-Host "DNS Entries Added to InterfaceIndex $InterfaceIndexNum"
 
         Get-NetIPConfiguration -InterfaceIndex $global:InterfaceIndexNum
     }
 
+    #(R)emove DNS Entries
     elseif ($WhichAction -eq 'R') {
         Write-Host 'Starting process to Remove DNS Entries and restore DHCP...'
         GetInterfaceIndex
@@ -37,11 +41,14 @@ do {
 
         ipconfig /all
     }
+
+    #(E)xit the program
     elseif ($WhichAction -eq 'E') {
         Write-Host "Exiting without performing any actions..."
         Return
     }    
 
+    #Error catching
     else {
         #$End = Read-Host -Prompt 'Invalid entry' + $WhichAction + ', press Enter to close...'
         Write-Warning "Invalid entry ""$WhichAction"", please enter A, R, or E..."
