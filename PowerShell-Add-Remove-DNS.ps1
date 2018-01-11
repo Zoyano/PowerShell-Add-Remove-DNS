6a34471e3e590c05f55af4042c70eabaf8d562bd
Zoyano <34747586+Zoyano@users.noreply.github.com>
2018-01-11 07:59:16 -0600

    Adding error checking (#1)
    * Update 5
    
    Added info to ReadMe.
    
    * Update 6
    
    Added exit on error for Add/Remove options.
    
    * Update 7
    
    Added an extra list of Backup DNS connections.

--- PowerShell-Add-Remove-DNS.ps1
+++ PowerShell-Add-Remove-DNS.ps1
@@ -2,11 +2,13 @@ Clear-Host
 
 <#************** Enter List of DNS IP's here for Adding **************#>
 $Global:DNSEntries = '10.200.160.179', '10.200.160.173', '10.200.200.33', '10.200.200.32'
+#$Global:DNSEntries = '10.80.72.4', '10.11.0.23' #Backup for VPN Connection
 
 <#************** Functions **************#>
 function AddRemove-DNSEntries ($x) {
     #Define Variables
     $InterfaceIndexNum = 0
+    $DefaultIndexNum = Get-DnsClientServerAddress -InterfaceAlias Ethernet -AddressFamily IPv4
 
     #Get-WmiObject win32_networkadapter -filter 'netconnectionstatus = 2' | select NetConnectionid, Name, Interfaceindex, NetConnectionStatus | Out-Host
     
@@ -14,21 +16,26 @@ function AddRemove-DNSEntries ($x) {
     Get-NetAdapter | Where-Object Status -eq 'Up' |
         Select-Object Name, InterfaceDescription, @{Name = "Interfaceindex"; Expression = {$_."ifIndex"}} , Status | Out-Host
 
-    $InterfaceIndexNum = Read-Host "Which Interfaceindex do you want to $x DNS entries for?"
+    $InterfaceIndexNum = Read-Host "Which Interfaceindex do you want to $x DNS entries for (E to default to Ethernet)?"
     Write-Host ""
+
+    #Use InterfaceIndex of Ethernet adapter if user entered E
+    if ($InterfaceIndexNum -eq 'E') {
+        $InterfaceIndexNum = $DefaultIndexNum.InterfaceIndex
+    }
 
     #if Add was passed to function, run add DNS entries process
     if ($x -eq 'Add') {
-        Set-DnsClientServerAddress -Interfaceindex $InterfaceIndexNum -ServerAddresses ($Global:DNSEntries)
+        Set-DnsClientServerAddress -Interfaceindex $InterfaceIndexNum -ServerAddresses ($Global:DNSEntries) -ErrorAction Stop
         #Set-DnsClientServerAddress -Interfaceindex $InterfaceIndexNum -ServerAddresses ($Global:DNSEntries) -WhatIf
         Write-Host "DNS Entries Added to InterfaceIndex $InterfaceIndexNum" -ForegroundColor green
     }
 
     #if Remove was passed to function, run remove DNS entries process
     elseif ($x -eq 'Remove') {
-        Set-DnsClientServerAddress -Interfaceindex $InterfaceIndexNum -ResetServerAddresses
+        Set-DnsClientServerAddress -Interfaceindex $InterfaceIndexNum -ResetServerAddresses -ErrorAction Stop
         #Set-DnsClientServerAddress -Interfaceindex $InterfaceIndexNum -ResetServerAddresses -WhatIf
-        Write-Host "DNS Entries Removed from InterfaceIndex $InterfaceIndexNum, DHCP restored" -ForegroundColor green
+        Write-Host "DNS Entries Removed from InterfaceIndex $InterfaceIndexNum, DHCP restored" -ForegroundColor Green
     }
 
     Get-NetIPConfiguration -InterfaceIndex $InterfaceIndexNum
@@ -41,19 +48,19 @@ do {
 
     #(A)dd DNS Entries
     if ($WhichAction -eq 'A') {
-        Write-Host 'Starting process to Add DNS Entries...'
+        Write-Host 'Starting process to Add DNS Entries...' -ForegroundColor Green
         AddRemove-DNSEntries('Add')
     }
 
     #(R)emove DNS Entries
     elseif ($WhichAction -eq 'R') {
-        Write-Host 'Starting process to Remove DNS Entries and restore DHCP...'
+        Write-Host 'Starting process to Remove DNS Entries and restore DHCP...' -ForegroundColor Green
         AddRemove-DNSEntries('Remove')
     }
 
     #(E)xit the program
     elseif ($WhichAction -eq 'E') {
-        Write-Host "Exiting without performing any actions..."
+        Write-Host "Exiting without performing any actions..." -ForegroundColor Red
         Return
     }    
 
